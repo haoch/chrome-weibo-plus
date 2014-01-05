42d29bd55fcd959ed555c84e8c0c9aa57a73718e
Hao Chen <cn.haochen@gmail.com>
2014-01-05 19:45:05 +0800

    rm source code

--- background.js
+++
@@ -1,9 +0,0 @@
-// Called when the user clicks on the browser action.
-// !!! not implemented now !!!
-chrome.browserAction.onClicked.addListener(function(tab) {
-	  // No tabs or host permissions needed!
-	  console.log('Turning ' + tab.url + ' red!');
-	  chrome.tabs.executeScript({
-	    code: 'document.body.style.backgroundColor="black"'
-	  });
-});
